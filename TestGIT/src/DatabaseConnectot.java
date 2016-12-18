255d1554470bcee4e95a3c7df41ed2ab31b3bf4d
Masood <hammad.masood92@gmail.com>
2016-12-18 18:26:28 +0530

    First commit with GIT

---
+++ TestGIT/src/DatabaseConnectot.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * @author Masood
+ *
+ */
+public class DatabaseConnectot {
+	
+	/* DUMMY CLASS :P */
+	
+	public static void main(String[] args) {
+		System.out.println("Testing GIT");
+	}
+
+}
