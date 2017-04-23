c7a7ed46cf6009f94558e45cb7a35fc6b362834a
Hammad Masood <hammadmasood92@gmail.com>
2017-04-23 13:01:31 +0530

    hh

--- TestGIT/src/DatabaseConnectot.java
+++ TestGIT/src/DatabaseConnectot.java
@@ -8,9 +8,6 @@
  */
 public class DatabaseConnectot {
 	
-	public static void main(String[] args) {
-		/* This is an attempt to create conflicts */
-	}
 	/* This is an attempt to create a conflict */
 	
 	/* DUMMY CLASS :P */
