5a1551af1ad82eb6e2bff93a392c00c177e8d3a4
Hammad Masood <hammadmasood92@gmail.com>
2017-04-23 13:04:44 +0530

    r

--- TestGIT/src/DatabaseConnectot.java
+++ TestGIT/src/DatabaseConnectot.java
@@ -8,13 +8,14 @@
  */
 public class DatabaseConnectot {
 	
+	/* This is an attempt to create classical remote conflict during pull request */
+	
+	public static void main(String[] args) {
+		/* This is an attempt to create conflicts */
+	}
 	/* This is an attempt to create a conflict */
 	
 	/* DUMMY CLASS :P */
-	
-	public static void main(String[] args) {
-		/* I am Happy @@ */
-	}
 	
 	/* I am an innocent commit */
 
