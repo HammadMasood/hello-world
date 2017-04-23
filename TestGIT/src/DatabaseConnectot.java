db3b9ac4e1f8c719b2595096822340da4f64acd8
HammadMasood <hammadmasood92@gmail.com>
2017-04-23 12:59:36 +0530

    Update DatabaseConnectot.java

--- TestGIT/src/DatabaseConnectot.java
+++ TestGIT/src/DatabaseConnectot.java
@@ -8,6 +8,8 @@
  */
 public class DatabaseConnectot {
 	
+	/* This is an attempt to create classical remote conflict during pull request */
+	
 	public static void main(String[] args) {
 		/* This is an attempt to create conflicts */
 	}
